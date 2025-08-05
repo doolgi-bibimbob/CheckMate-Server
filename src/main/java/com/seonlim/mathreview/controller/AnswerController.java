8b347be13debf2813cdd42b1c2582f2c6a6333cf
me <kurikame73@gmail.com>
2025-08-05 21:52:01 +0900

    refactor: update answer submission process to use AnswerSubmit DTO and refactor related methods

--- src/main/java/com/seonlim/mathreview/controller/AnswerController.java
+++ src/main/java/com/seonlim/mathreview/controller/AnswerController.java
@@ -2,10 +2,7 @@ package com.seonlim.mathreview.controller;
 
 import com.seonlim.mathreview.dto.AnswerDetail;
 import com.seonlim.mathreview.dto.AnswerSubmit;
-import com.seonlim.mathreview.dto.AnswerSubmitRequest;
-import com.seonlim.mathreview.dto.AnswerSubmitRequestListTest;
 import com.seonlim.mathreview.service.AnswerService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class AnswerController {
 
     private final AnswerService answerService;
-
-    @PostMapping("/submit-test")
-    public ResponseEntity<Void> submitAnswer(@RequestBody AnswerSubmitRequest request) {
-        answerService.submit(request);
-        return ResponseEntity.ok().build();
-    }
 
     @PostMapping("/submit")
     public ResponseEntity<Void> submitAnswerListTest(@RequestBody AnswerSubmit request) {
