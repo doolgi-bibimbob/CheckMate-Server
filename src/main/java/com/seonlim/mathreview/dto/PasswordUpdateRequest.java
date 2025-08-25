2ab418e194f767b3fe2344becf1e8a58c25f692d
me <kurikame73@gmail.com>
2025-08-25 22:21:11 +0900

    feat: 비밀번호 변경 이메일 인증

--- src/main/java/com/seonlim/mathreview/dto/PasswordUpdateRequest.java
+++ src/main/java/com/seonlim/mathreview/dto/PasswordUpdateRequest.java
@@ -9,5 +9,6 @@ public record PasswordUpdateRequest(
 //                regexp = "^(?=.*[A-Za-z])(?=.*\\d).{8,20}$",
 //                message = "비밀번호는 영문자와 숫자를 모두 포함해 8~20자여야 합니다."
 //        )
+        String email,
         String newPassword
 ) {}
